af4e86adb7d0f4af2cdd7f2a91b5e2d588038ea8
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-08-03 21:35:12 +0930

    initial commit

---
+++ config/seeds.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const db = require('./connection');
+const { User } = require('../models');
+
+db.once('open', async () => {
+
+  await User.create({
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+  });
+
+  await User.create({
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345'
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
