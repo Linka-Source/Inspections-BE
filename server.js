a5e52cbd6bbd1a4b4b2435ac0070e8d37d2af232
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-08-12 19:24:15 +0930

    merge todos

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const { ApolloServer } = require('apollo-server-express');
-
+const {Todo} = require("./models")
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-
+const cors = require('cors')
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -16,6 +16,39 @@ const server = new ApolloServer({
 });
 
 app.use(logger('dev'));
+app.use(cors())
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+      
+      res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+  const todo = new Todo({
+      text: req.body.text
+  });
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id)
+
+  res.json(result);
+});
+
+app.get('/todo/complete/:id', async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.complete = !todo.complete;
+
+  todo.save();
+
+  res.json(todo);
+});
 
 server.applyMiddleware({ app });
 
